08ac2ad4b13b538cde2bb60c5ac3e21f5762376e
snaipasomoh <myboloto99@gmail.com>
2019-11-20 00:53:47 +0300

    Lagrange and Newton polinomial calculation added

--- 3/lab3.cpp
+++ 3/lab3.cpp
@@ -2,6 +2,61 @@
 #include <cmath>
 #include "simsym.hpp"
 
-int main (int argc, char **argv) {
-	
+simsym getLagr (std::vector<double> const &x, std::vector<double> const &y){
+	simsym res(0);
+	simsym xVar({0, 1});
+	size_t len = x.size();
+	for (size_t i = 0; i < len; i++){
+		simsym temp(y[i]);
+		for (size_t j = 0; j < len; j++){
+			if (i == j) { continue; }
+			temp = temp * (xVar - x[j]) / (x[i] - x[j]);
+		}
+		res = res + temp;
+	}
+	return res;
+}
+
+simsym getNewt (std::vector<double> const &x, std::vector<double> const &y){
+	std::vector<double> currLevelDivDiff = y;
+	simsym res(y[0]);
+	simsym xVar({0, 1});
+	simsym mult(1);
+	for (size_t i = 1; i < x.size(); i++){
+		mult = mult * (xVar - x[i - 1]);
+		std::vector<double> temp;
+		{
+			for (size_t j = 0; j < currLevelDivDiff.size() - 1; j++){
+				temp.push_back((currLevelDivDiff[j] - currLevelDivDiff[j + 1])
+								/ (x[j] - x[j + i]));
+			}
+		}
+		currLevelDivDiff = temp;
+		res = res + mult * currLevelDivDiff[0];
+	}
+	return res;
+}
+
+double getValInX (simsym const &pol, double x){
+	double res = 0;
+	for (size_t i = 0; i < pol.size(); i++){
+		res += pol[i] * std::pow(x, i);
+	}
+	return res;
+}
+
+int main (int argc, char **argv){
+	std::vector<double> xVal {0, M_PI/6, 2*M_PI/6, 3*M_PI/6};
+	std::vector<double> yVal;
+	double X = M_PI/4;
+	for (size_t i = 0; i < xVal.size(); i++){
+		yVal.push_back(cos(xVal[i]));
+	}
+	simsym lagr = getLagr(xVal, yVal);
+	std::cout << "Value of Lagrange polynomial in X = " << X << " : ";
+	std::cout << getValInX(lagr, X) << std::endl;
+	std::cout << std::endl;
+	std::cout << "Value of Newton polynomial in X = " << X << " : ";
+	simsym newt = getNewt(xVal, yVal);
+	std::cout << getValInX(newt, X) << std::endl;
 }
--- 3/simsym.hpp
+++ 3/simsym.hpp
@@ -5,15 +5,15 @@
 class simsym{
 	std::vector<double> data;
 public:
-	simsym (double n) : data(std::vector<double> {n}) {}
+	simsym (double n) : data(std::vector<double> {n}){}
 
-	simsym (std::vector<double> const &n) : data(n) { this->cutZeros(); }
+	simsym (std::vector<double> const &n) : data(n){ this->cutZeros(); }
 
-	size_t size() const {
+	size_t size() const{
 		return data.size();
 	}
 
-	double operator[] (size_t index) const {
+	double operator[] (size_t index) const{
 		return data[index];
 	}
 
@@ -21,11 +21,11 @@ public:
 		return data[index];
 	}
 
-	std::vector<double> getCoefVec () const {
+	std::vector<double> getCoefVec () const{
 		return data;
 	}
 
-	simsym &cutZeros () {
+	simsym &cutZeros (){
 		std::vector<double>::iterator firstZero = data.end();
 		for (auto iter = data.end() - 1; *iter == 0; firstZero = iter--){}
 		data.erase(firstZero, data.end());
@@ -33,7 +33,7 @@ public:
 	}
 };
 
-simsym operator+ (simsym const &a, simsym const &b) {
+simsym operator+ (simsym const &a, simsym const &b){
 	if (a.size() > b.size()){
 		std::vector<double> newData (a.getCoefVec());
 		for (size_t i = 0; i < b.size(); i++){
@@ -48,7 +48,7 @@ simsym operator+ (simsym const &a, simsym const &b) {
 	return simsym(newData).cutZeros();
 }
 
-simsym operator- (simsym const &a, simsym const &b) {
+simsym operator- (simsym const &a, simsym const &b){
 	auto coefs = b.getCoefVec();
 	for (double &i : coefs){
 		i *= -1;
@@ -56,13 +56,21 @@ simsym operator- (simsym const &a, simsym const &b) {
 	return a + coefs;
 }
 
-simsym operator* (simsym const &a, simsym const &b) {
+simsym operator* (simsym const &a, simsym const &b){
 	size_t len = a.size() + b.size();
 	std::vector<double> newData (len, 0);
 	for (size_t i = 0; i < a.size(); i++){
 		for (size_t j = 0; j < b.size(); j++){
 			newData[i + j] += a[i] * b[j];
 		}
+	}
+	return simsym(newData).cutZeros();
+}
+
+simsym operator/ (simsym const &a, double const b){
+	std::vector<double> newData = a.getCoefVec();
+	for (auto &i : newData){
+		i /= b;
 	}
 	return simsym(newData).cutZeros();
 }
