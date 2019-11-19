2ff90203b217cd04e60ff8d4b02b494e73490c5d
snaipasomoh <myboloto99@gmail.com>
2019-11-20 02:40:35 +0300

    Plotting via gnuplot added

--- 3/lab3.cpp
+++ 3/lab3.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <cmath>
+#include <cstdio>
 #include "simsym.hpp"
 
 simsym getLagr (std::vector<double> const &x, std::vector<double> const &y){
@@ -59,4 +61,35 @@ int main (int argc, char **argv){
 	std::cout << "Value of Newton polynomial in X = " << X << " : ";
 	simsym newt = getNewt(xVal, yVal);
 	std::cout << getValInX(newt, X) << std::endl;
+
+	FILE *pipe = popen("gnuplot -persist", "w");
+	if (pipe){
+		size_t nDots = 20;
+		double step = (xVal[xVal.size() - 1] - xVal[0]) / nDots;
+		std::string f, l, n;
+		for(size_t i = 0; i <= nDots; i++){
+			double x = xVal[0] + i * step;
+			f += std::to_string(x) + " " + std::to_string(cos(x)) + "\n";
+			l += std::to_string(x) + " " +
+									std::to_string(getValInX(lagr, x)) + "\n";
+			n += std::to_string(x) + " " +
+									std::to_string(getValInX(newt, x)) + "\n";
+		}
+
+
+		fprintf(pipe, "set title \"Lab 3\"\n");
+		fprintf(pipe, "set xrange [%f:%f]\n", xVal[0], xVal[xVal.size() - 1]);
+		fprintf(pipe, "set autoscale y\n");
+		fprintf(pipe, "plot '-' title \"y=cos(x)\" w l lc 1 lw 1,\
+							'' title \"y=L_n(x)\" w l lc 2 lw 1,\
+							'' title \"y=P_n(x)\" w l lc 3 lw 1\n");
+		fprintf(pipe, "%se\n%se\n%se\n", f.c_str(), l.c_str(), n.c_str());
+		fflush(pipe);
+
+		std::cin.clear();
+		std::cin.ignore(std::cin.rdbuf()->in_avail());
+		std::cin.get();
+
+		pclose(pipe);
+	}
 }
