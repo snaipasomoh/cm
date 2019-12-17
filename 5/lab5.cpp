1414757511e933ff9f3db3675fe3fc8cb57ef99e
snaipasomoh <myboloto99@gmail.com>
2019-12-18 01:09:44 +0300

    plotting added for lab5

--- 5/lab5.cpp
+++ 5/lab5.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <simsym>
-// #include <eigen3/Eigen/Dense>
+#include <cstdio>
+#include "simsym.hpp"
+#include <eigen3/Eigen/Dense>
 
 Eigen::VectorXd solveLS (Eigen::MatrixXd const &A, Eigen::VectorXd const &b){
 	return A.colPivHouseholderQr().solve(b);
@@ -10,28 +11,30 @@ Eigen::VectorXd solveLS (Eigen::MatrixXd const &A, Eigen::VectorXd const &b){
 
 std::vector<double> ssm (std::vector<double> const &x,
 						 std::vector<double> const &y, size_t k){
-	std::vector<double> xPows (0, k * k + 1);
-	std::vector<double> yxPows (0, k + 1);
+	std::vector<double> xPows (2 * k + 1, 0);
+	std::vector<double> yxPows (k + 1, 0);
 	size_t len = x.size();
+
 	for (size_t i = 0; i < len; i++){
-		for (size_t j = 0; j < k * k + 1; j++){
+		for (size_t j = 0; j < 2 * k + 1; j++){
 			xPows[j] += std::pow(x[i], j);
 		}
 		for (size_t j = 0; j < k + 1; j++){
 			yxPows[j] += y[i] * std::pow(x[i], j);
 		}
 	}
-	Eigen::MatrixXd A(k, k);
-	Eigen::VectorXd b(k);
-	for (size_t i = 0; i < k; i++){
-		for (size_t j = 0; j < k; j++){
+	// std::cout << "hello" << std::endl;
+	Eigen::MatrixXd A(k + 1, k + 1);
+	Eigen::VectorXd b(k + 1);
+	for (size_t i = 0; i < k + 1; i++){
+		for (size_t j = 0; j < k + 1; j++){
 			A(i, j) = xPows[i + j];
 		}
 		b(i) = yxPows[i];
 	}
 	auto sol = solveLS(A, b);
 	std::vector<double> res;
-	for (size_t i = 0; i < k; i++){
+	for (size_t i = 0; i < k + 1; i++){
 		res.push_back(sol(i));
 	}
 	return res;
@@ -51,4 +54,38 @@ int main (int argc, char **argv){
 	std::vector<double> F1 = ssm(xVal, yVal, 1);
 	std::vector<double> F2 = ssm(xVal, yVal, 2);
 	std::vector<double> F3 = ssm(xVal, yVal, 3);
+
+
+
+	FILE *pipe = popen("gnuplot -persist", "w");
+	if (pipe){
+		size_t nDots = 100;
+		double step = (xVal[xVal.size() - 1] - xVal[0] + 2) / nDots;
+		std::string f1, f2, f3;
+		for(size_t i = 0; i <= nDots; i++){
+			double x = xVal[0] - 1 + i * step;
+			f1 += std::to_string(x) + " " +
+									std::to_string(getValInX(F1, x)) + "\n";
+			f2 += std::to_string(x) + " " +
+									std::to_string(getValInX(F2, x)) + "\n";
+			f3 += std::to_string(x) + " " +
+									std::to_string(getValInX(F3, x)) + "\n";
+		}
+
+
+		fprintf(pipe, "set title \"Lab 5\"\n");
+		fprintf(pipe, "set xrange [%f:%f]\n", xVal[0], xVal[xVal.size() - 1]);
+		fprintf(pipe, "set autoscale y\n");
+		fprintf(pipe, "plot '-' title \"y=F1(x)\" w l lc 1 lw 10,\
+							'' title \"y=F2(x)\" w l lc 2 lw 10,\
+							'' title \"y=F3(x)\" w l lc 4 lw 10\n");
+		fprintf(pipe, "%se\n%se\n%se\n", f1.c_str(), f2.c_str(), f3.c_str());
+		fflush(pipe);
+
+		std::cin.clear();
+		std::cin.ignore(std::cin.rdbuf()->in_avail());
+		std::cin.get();
+
+		pclose(pipe);
+	}
 }
