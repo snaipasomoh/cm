549fe900517cb08b6556fd698ae525db2b38cc5c
snaipasomoh <myboloto99@gmail.com>
2019-12-17 22:14:42 +0300

    lab5 added

---
+++ 5/lab5.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+double mFunc (double x){
+	return x / std::pow((3 * x + 4), 2);
+}
+
+double rectMethod (std::vector<double> x, double dx, double y (double)){
+	double res = 0;
+	for (double i : x){
+		res += y(i) * dx;
+	}
+	return res;
+}
+
+double leftRect (double a, double b, double dx, double y (double)){
+	std::vector<double> x;
+	while (a < b){
+		x.push_back(a);
+		a += dx;
+	}
+	return rectMethod(x, dx, y);
+}
+
+double rightRect (double a, double b, double dx, double y (double)){
+	std::vector<double> x;
+	while (a < b){
+		x.push_back(a + dx);
+		a += dx;
+	}
+	return rectMethod(x, dx, y);
+}
+
+double middleRect (double a, double b, double dx, double y (double)){
+	std::vector<double> x;
+	while (a < b){
+		x.push_back(a + dx / 2);
+		a += dx;
+	}
+	return rectMethod(x, dx, y);
+}
+
+double trapezMethod (double a, double b, double dx, double y (double)){
+	double res = 0;
+	while (a < b){
+		res += (y(a) + y(a + dx)) * dx / 2;
+		a += dx;
+	}
+	return res;
+}
+
+double simpsonsMethod (double a, double b, double dx, double y (double)){
+	double res = 0;
+	while (a < b){
+		res += (y(a) + 4 * y(a + dx / 2) + y(a + dx)) * dx / 6;
+		a += dx;
+	}
+	return res;
+}
+
+int main (int argc, char **argv){
+	std::cout << leftRect(0, 4, 1, mFunc) << std::endl;
+	std::cout << rightRect(0, 4, 1, mFunc) << std::endl;
+	std::cout << middleRect(0, 4, 1, mFunc) << std::endl;
+	std::cout << trapezMethod(0, 4, 1, mFunc) << std::endl;
+	std::cout << simpsonsMethod(0, 4, 1, mFunc) << std::endl;
+}
