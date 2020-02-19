26e95d25f8faed7542337ac5e0786a5c6d96e9dc
snaipasomoh <myboloto99@gmail.com>
2020-02-19 11:46:09 +0300

    lab8 added

---
+++ 8/lab8.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+struct eqVectors{
+	std::vector<double> a;
+	std::vector<double> b;
+	std::vector<double> c;
+	std::vector<double> d;
+};
+
+std::vector<double> thomasMethod (eqVectors const &vec){
+	std::vector<double> res;
+	std::vector<double> nd {vec.d[0] / vec.b[0]};
+	std::vector<double> nc {vec.c[0] / vec.b[0]};
+	size_t len = vec.d.size();
+
+	for (size_t i = 1; i < len; i++){
+		nd.push_back((vec.d[i] - vec.a[i] * nd[i - 1]) /
+											(vec.b[i] - vec.a[i] * nc[i - 1]));
+		if (i == len - 1){
+			continue;
+		}
+		nc.push_back(vec.c[i] / (vec.b[i] - vec.a[i] * nc[i - 1]));
+	}
+
+	res.push_back(nd[len - 1]);
+	for (size_t i = 1; i < len; i++){
+		res.push_back(nd[len - i - 1] - nc[len - i - 1] * res[i - 1]);
+	}
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+int main (int argc, char **argv){
+	eqVectors foo;
+	foo.a = {0, -7, -7, 4, -6};
+	foo.b = {-12, -11, 21, -13, 14};
+	foo.c = {-7, -3, -8, 5, 0};
+	foo.d = {-102, -92, -65, 38, -12};
+
+	std::vector<double> res = thomasMethod(foo);
+	for (auto i : res){
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
