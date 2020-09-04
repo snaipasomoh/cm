7ae813b8b766823436ef8a181b83f78656ef9a8c
snaipasomoh <myboloto99@gmail.com>
2020-09-04 03:07:44 +0300

    added recalc for y''

--- 16/lab16.cpp
+++ 16/lab16.cpp
@@ -17,16 +17,25 @@ void solve (double x0, double x1, double y0, double z0, double h = H1){
 	double z2 = f(x0, y0, z0);
 	double z1 = z0;
 	double y = y0;
+	double z2t;
+	double z1t;
+	double yt;
 	std::cout << "H = " << h << std::endl;
 	std::cout << "x\ty\ty'" << std::setprecision(4) << std::endl;
 	for (double x = x0; x <= x1 + h / 2; x += h){
 		std::cout << x << "\t" << y << "\t" << z1 << std::endl;
-		z2 = z2 + h * f(x, y, z1);
-		z1 = z1 + h * z2;
-		y = y + h * z1;
+		z2t = z2 + h * f(x, y, z1);
+		z1t = z1 + h * z2;
+		yt = y + h * z1;
+
+		z2 = z2 + h / 2 * (f(x, y, z1) + f(x + h, yt, z1t));
+		z1 = z1t;
+		y = yt;
 	}
 }
 
 int main (int argc, char **argv){
+	solve(1, 2, 1, 2);
+	std::cout << std::endl;
 	solve(1, 2, 1, 2, H2);
 }
