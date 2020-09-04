a096a3736fb58c2f4ca24a113870a594a87434b8
snaipasomoh <myboloto99@gmail.com>
2020-09-04 04:06:04 +0300

    lab14 fix

--- 14/lab14.cpp
+++ 14/lab14.cpp
@@ -18,7 +18,7 @@ void foo(double x0, double x1, double y0, double h = H1){
 		double k2 = h * f(x + h / 2, y + k1 / 2);
 		double k3 = h * f(x + h / 2, y + k2 / 2);
 		double k4 = h * f(x + h, y + k3);
-		y = y + (k1 + k2 + k3 + k4) / 6;
+		y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
 	}
 }
 
